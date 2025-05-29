2789e8dbe1bc2e51e1c114896191c2be5e594829
Im-Sena <sena.saeki@gmail.com>
2025-05-29 21:57:40 +0900

    グラフ描画の際にリセットされていなかった点を修正

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 #ファイル名を指定
 #あとでfor文でまとめるかつはコマンドライン引数で指定できるようにする
-file1 = "samples/sample1.pgm" #女
-file2 = "samples/sample2.pgm" #光
-file3 = "samples/sample3.pgm" #文字
-
+file0 = "samples/sample1.pgm" #女
+file1 = "samples/sample2.pgm" #光
+file2 = "samples/sample3.pgm" #文字
+file3 = "samples/sample4.jpg" #自身で選んだ画像
+files = [file0, file1, file2, file3]
 #画像の読み込み
-#後でfor文でまとめる
-img = []
-img1 = cv2.imread(str(file1))
-img2 = cv2.imread(str(file2))
-img3 = cv2.imread(str(file3))
-
-img = [img1, img2, img3]
-
+imgs = []
+for i in range(4):
+    imgs.append(cv2.imread(str(files[i])))
 #画像をHSV方式に変換
 #輝度に使うのはv
-
 hsv = []
 h,s,v=[],[],[]
-for i in range(3):
-    hsv_img = cv2.cvtColor(img[i], cv2.COLOR_BGR2HSV)
+for i in range(len(imgs)):
+    hsv_img = cv2.cvtColor(imgs[i], cv2.COLOR_BGR2HSV)
     hsv.append(hsv_img)
     hi, si, vi = cv2.split(hsv_img)  # ←ここが正しい
     h.append(hi)
@@ -45,6 +38,8 @@ for i in range(3):
     hist128_csv = ",".join(map(str, hist128)) + "\n"
     with open(f"hist128_{i}.csv", "w") as f:
         f.write(hist128_csv)
+    
+    plt.clf()  # グラフをクリア
 
 
 
